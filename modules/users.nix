0f911715356d0db47c478d4ceb76812cf5775582
Tyler Montgomery <tylerfixer@thecubed.com>
2025-06-26 16:42:17 -0500

    nopasswd for wheel users

--- modules/users.nix
+++ modules/users.nix
@@ -3,14 +3,15 @@ let
 	kioskUsers = config.modules.nixos-kiosk.users;
 in {
 	config = {
+		security.sudo.wheelNeedsPassword = false;
 	  users.users = {
 			"${kioskUsers.admin}" = {
-		      isNormalUser = true;
-		      extraGroups = [ "wheel" ];
-		      packages = with pkgs; [
-		        vim
-		      ];
-		    };
+				isNormalUser = true;
+				extraGroups = [ "wheel" ];
+				packages = with pkgs; [
+					vim
+				];
+			};
 	
 			"${kioskUsers.kiosk}" = {
 			  isNormalUser = true;
